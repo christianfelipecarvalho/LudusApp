0c405ca02894c820d49b44407f52e0903f96639b
christianfelipecarvalho <christianfelipecarvalho@gmail.com>
2025-02-20 11:38:16 -0300

    Ajuste urls webhosts

--- LudusApp/Program.cs
+++ LudusApp/Program.cs
@@ -11,7 +11,8 @@ using Microsoft.IdentityModel.Tokens;
 
 
 var builder = WebApplication.CreateBuilder(args);
-builder.WebHost.UseUrls("http://0.0.0.0:880", "https://0.0.0.0:443");
+builder.WebHost.UseUrls("http://0.0.0.0:80");
+
 
 var connectionString = builder.Configuration.GetConnectionString("LudusAppContext");
 
